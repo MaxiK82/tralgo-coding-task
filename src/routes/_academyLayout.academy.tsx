8b5d7efc697105601e13e5f5b6829b434403ff5f
Max <maxikrause82@gmail.com>
2025-05-24 19:36:37 +0200

    (TCT): addind breadcrumb nav to the academy layout

--- src/routes/_academyLayout.academy.tsx
+++ src/routes/_academyLayout.academy.tsx
@@ -1,4 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { HomeIcon } from '@heroicons/react/20/solid'
+
+const pages = [
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Project Nero', href: '#', current: true },
+]
 
 export const Route = createFileRoute('/_academyLayout/academy')({
   component: RouteComponent,
@@ -8,7 +14,43 @@ function RouteComponent() {
   return (
     <>
       <main className="-mt-24 pb-8">
-        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-[1920px] lg:px-8">
+        <div className='mx-auto max-w-3xl lg:max-w-[1920px]'>
+          <nav aria-label="Breadcrumb" className="flex border-b border-gray-200 bg-white max-w-3xl px-4 sm:px-6 lg:px-8 lg:max-w-[1856px] mb-8 mx-8 rounded-md">
+            <ol role="list" className="mx-auto flex w-full space-x-4 px-4 sm:px-6 lg:px-8">
+              <li className="flex">
+                <div className="flex items-center">
+                  <a href="/academy" className="text-gray-400 hover:text-gray-500">
+                    <HomeIcon aria-hidden="true" className="size-5 shrink-0" />
+                    <span className="sr-only">Home</span>
+                  </a>
+                </div>
+              </li>
+              {pages.map((page) => (
+                <li key={page.name} className="flex">
+                  <div className="flex items-center">
+                    <svg
+                      fill="currentColor"
+                      viewBox="0 0 24 44"
+                      preserveAspectRatio="none"
+                      aria-hidden="true"
+                      className="h-full w-6 shrink-0 text-gray-200"
+                    >
+                      <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                    </svg>
+                    <a
+                      href={page.href}
+                      aria-current={page.current ? 'page' : undefined}
+                      className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                    >
+                      {page.name}
+                    </a>
+                  </div>
+                </li>
+              ))}
+            </ol>
+          </nav>
+        </div>
+        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:px-8 lg:max-w-[1920px]">
           <h1 className="sr-only">Academy</h1>
           {/* Main 3 column grid */}
           <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
