fc257b8e052c8d47da1281e5be4c95ae93033213
Max <maxikrause82@gmail.com>
2025-05-24 19:11:40 +0200

    (TCT): updating the academy layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,75 +4,75 @@ import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Footer } from '../components'
 
 const navigation = [
-    { name: 'Startseite', href: '/' },
-    { name: 'Ergebnisse', href: '/results' },
-    { name: 'Academy', href: '/academy' },
+	{ name: 'Startseite', href: '/' },
+	{ name: 'Ergebnisse', href: '/results' },
+	{ name: 'Academy', href: '/academy' },
 ]
 
 export const Layout = ({ children }: any) => (
-    <div className="min-h-full">
-        <div className="bg-tralgo-gray-950 pb-32">
-            <Disclosure as="nav" className="bg-tralgo-gray-950">
-                <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="p-3">
-                        <div className="flex h-16 items-center justify-between px-4 sm:px-0">
-                            <div className="flex items-center">
-                                <div className="shrink-0">
-                                    <a href='/' target="_self" rel="noopener noreferrer">
-                                        <img
-                                            alt="Your Company"
-                                            src="/img/logo.png"
-                                            className="w-20"
-                                        />
-                                    </a>
-                                </div>
-                                <div className="hidden md:block">
-                                    <div className="ml-10 flex items-baseline space-x-4 text-white">
-                                        {navigation.map((item) => (
-                                            <Link
-                                                key={item.name}
-                                                to={item.href}
-                                                className='rounded-md px-3 py-2 text-sm font-bold hover:text-tralgo-blue-500'
-                                                activeOptions={{ exact: true }}
-                                                activeProps={{
-                                                    className: 'text-tralgo-blue-500',
-                                                }}
-                                            >
-                                                {item.name}
-                                            </Link>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="-mr-2 flex md:hidden">
-                                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-tralgo-gray-950 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                                    <span className="absolute -inset-0.5" />
-                                    <span className="sr-only">Open main menu</span>
-                                    <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                                    <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-                                </DisclosureButton>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <DisclosurePanel className="border-b border-gray-700 md:hidden">
-                    <div className="space-y-1 px-2 py-3 sm:px-3">
-                        {navigation.map((item) => (
-                            <DisclosureButton
-                                key={item.name}
-                                as={Link}
-                                href={item.href}
-                                className='rounded-md px-3 py-2 text-sm font-bold text-white hover:text-tralgo-blue-500'
-                            >
-                                {item.name}
-                            </DisclosureButton>
-                        ))}
-                    </div>
-                </DisclosurePanel>
-            </Disclosure>
-        </div>
-        {children}
-        <Footer />
-    </div>
+	<div className="min-h-full">
+		<div className="bg-tralgo-gray-950 pb-32">
+			<Disclosure as="nav" className="bg-tralgo-gray-950">
+				<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div className="p-3">
+						<div className="flex h-16 items-center justify-between px-4 sm:px-0">
+							<div className="flex items-center">
+								<div className="shrink-0">
+									<a href='/' target="_self" rel="noopener noreferrer">
+										<img
+											alt="Your Company"
+											src="/img/logo.png"
+											className="w-20"
+										/>
+									</a>
+								</div>
+								<div className="hidden md:block">
+									<div className="ml-10 flex items-baseline space-x-4 text-white">
+										{navigation.map((item) => (
+											<Link
+												key={item.name}
+												to={item.href}
+												className='rounded-md px-3 py-2 text-lg hover:text-tralgo-blue-500'
+												activeOptions={{ exact: true }}
+												activeProps={{
+													className: 'text-tralgo-blue-500',
+												}}
+											>
+												{item.name}
+											</Link>
+										))}
+									</div>
+								</div>
+							</div>
+							<div className="-mr-2 flex md:hidden">
+								<DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-tralgo-gray-950 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
+									<span className="absolute -inset-0.5" />
+									<span className="sr-only">Open main menu</span>
+									<Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+									<XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+								</DisclosureButton>
+							</div>
+						</div>
+					</div>
+				</div>
+				<DisclosurePanel className="border-b border-gray-700 md:hidden">
+					<div className="space-y-1 px-2 py-3 sm:px-3">
+						{navigation.map((item) => (
+							<DisclosureButton
+								key={item.name}
+								as={Link}
+								href={item.href}
+								className='rounded-md px-3 py-2 text-sm font-bold text-white hover:text-tralgo-blue-500'
+							>
+								{item.name}
+							</DisclosureButton>
+						))}
+					</div>
+				</DisclosurePanel>
+			</Disclosure>
+		</div>
+		{children}
+		<Footer />
+	</div>
 
 )
--- src/routes/_academyLayout.academy.tsx
+++ src/routes/_academyLayout.academy.tsx
@@ -8,15 +8,15 @@ function RouteComponent() {
   return (
     <>
       <main className="-mt-24 pb-8">
-        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h1 className="sr-only">Page title</h1>
+        <div className="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-[1920px] lg:px-8">
+          <h1 className="sr-only">Academy</h1>
           {/* Main 3 column grid */}
           <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
             {/* Left column */}
             <div className="grid grid-cols-1 gap-4 lg:col-span-2">
               <section aria-labelledby="section-1-title">
                 <h2 id="section-1-title" className="sr-only">
-                  Section title
+                  Academy Lernvideo
                 </h2>
                 <div className="overflow-hidden rounded-lg bg-white shadow-sm">
                   <div className="p-6">
@@ -30,7 +30,7 @@ function RouteComponent() {
             <div className="grid grid-cols-1 gap-4">
               <section aria-labelledby="section-2-title">
                 <h2 id="section-2-title" className="sr-only">
-                  Section title
+                  Navigation im Themem-Bereich
                 </h2>
                 <div className="overflow-hidden rounded-lg bg-white shadow-sm">
                   <div className="p-6">{/* Your content */}</div>
